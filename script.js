7d591247cf5519a4d4d4eda119594cabad18c9d2
vineethreddy899 <83892329+vineethreddy899@users.noreply.github.com>
2021-05-26 16:50:29 +0530

    porotfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".spltext", {
+    strings: ["Developer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".spltext-2", {
+    strings: ["Developer","Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+
